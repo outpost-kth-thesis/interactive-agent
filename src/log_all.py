43cdd6d2349a4a8eb241cbf270f986bcd5ee5e97
ohshitnotgood <praanto@icloud.com>
2025-04-18 17:26:34 +0200

    finally works somehow, do not touch

--- src/log_all.py
+++
@@ -1,5 +0,0 @@
-def request(flow):
-    print(f">>> Request: {flow.request.method} {flow.request.url}")
-
-def response(flow):
-    print(f"<<< Response: {flow.response.status_code} {flow.request.url}")
--- src/main.py
+++ src/main.py
@@ -2,36 +2,49 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.chrome.options import Options
-import os
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+
+chromedriver_path = "/usr/bin/chromedriver"
 options = Options()
-options.add_experimental_option("detach", True)
-os.environ["HTTP_PROXY"] = "http://127.0.0.1:8080"
-os.environ["HTTPS_PROXY"] = "http://127.0.0.1:8080"
-# options.add_argument("--proxy-server=http://127.0.0.1:8080")
+options.add_experimental_option("detach", False)
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--user-data-dir=/tmp/chrome-data") 
+options.add_argument("--enable-logging")
+options.add_argument("--v=1")
+options.add_argument("--disable-infobars")  # Disables the info bars
+options.add_argument("--disable-extensions")  # Disables extensions
 
+service = Service(executable_path=chromedriver_path)
+
+options.add_argument("--disable-blink-features=AutomationControlled")
 driver = webdriver.Chrome(options=options)
 
 driver.get("https://kth.se/")
 
-driver.maximize_window()
 
-combined_regex = '''//button | 
-//input[@type='button' or @type='submit' or @type='reset' or @type='text'] | 
-//textarea'''
-elements = driver.find_elements(By.XPATH, combined_regex)
+driver.close()
+# combined_regex = '''//button | 
+# //input[@type='button' or @type='submit' or @type='reset' or @type='text'] | 
+# //textarea'''
+# elements = driver.find_elements(By.XPATH, combined_regex)
 
-for el in elements:
-    parent1 = el.find_element(By.XPATH, "..")
-    parent2 = el.find_element(By.XPATH, "../..")
+# for el in elements:
+#     parent1 = el.find_element(By.XPATH, "..")
+#     parent2 = el.find_element(By.XPATH, "../..")
     
-    print("---")
-    print("Element HTML:")
-    print(el.get_attribute("outerHTML"))
+#     print("---")
+#     print("Element HTML:")
+#     print(el.get_attribute("outerHTML"))
     
-    print("Parent 1 HTML:")
-    print(parent1.get_attribute("outerHTML"))
+#     print("Parent 1 HTML:")
+#     print(parent1.get_attribute("outerHTML"))
 
-    print("Parent 2 HTML:")
-    print(parent2.get_attribute("outerHTML"))
+#     print("Parent 2 HTML:")
+#     print(parent2.get_attribute("outerHTML"))
 
